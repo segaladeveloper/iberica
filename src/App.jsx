6e338127867ced0b051515b7090b59c487443c76
segaladeveloper <129000812+segaladeveloper@users.noreply.github.com>
2023-03-26 16:45:12 -0300

    Correção

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css';
-import Features from './Features/Features';
 import { useCurrentUser } from 'thin-backend-react';
-import { updateRecord, createRecord } from 'thin-backend';
 import { useState } from 'react';
-import Header from './Header/Header';
-import { query } from 'thin-backend';
-import { useQuery } from 'thin-backend-react';
-import { Snackbar } from '@mui/material';
-import Dealhistory from './DealHistory/DealHistory';
 import Constantes from './Constantes';
-import PurchaseModal from './PurchaseModal/PurchaseModal';
 
 function App() {
   const [pagina, setPagina] = useState(Constantes.PAGINAS.HOME);
-  const cursos = useQuery(query('curso').orderByDesc('id'));
 
   return (
     <div className="App">
