41bc3181898278cc0a33dc500774bff95b32def1
segaladeveloper <129000812+segaladeveloper@users.noreply.github.com>
2023-03-26 16:42:32 -0300

    1.0

---
+++ src/Cursos/Cursos.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import './style.css';
+import { query } from 'thin-backend';
+
+const Cursos = () => {
+  const [cursos, setCursos] = useState([]);
+
+  useEffect(() => {
+    query('curso').fetch().then(results => {
+      setCursos(results)
+    })
+  }, []);
+
+   return (
+    <div>
+      <h1>Meus cursos</h1>
+      {cursos?.map(curso => {
+        return <h2>{curso}</h2>
+      })}
+    </div>
+  );
+
+}
+
+export default Cursos
